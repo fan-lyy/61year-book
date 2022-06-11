e16e03f7220cc92ce6b9b1219c9d9e7932893371
GuEr61 <fan77782010@qq.com>
2022-06-11 10:34:37 +0800

    加强完善框架

--- js/replace.js
+++ js/replace.js
@@ -1,11 +1,19 @@
 var page=1;
 function uppage(){
-    if(page>1)page--;
-    flushpage();
+    if(page>1)
+    {
+        page--;
+        flushpage();
+    }
+    else alert("这是第一页了！");
 }
 function downpage(){
-    if(page<1)page++;
-    flushpage();
+    if(page<1)
+    {   
+        page++;
+        flushpage();
+    }
+    else alert("这是最后一页了！");
 }
 function flushpage(){
     $("#hsize").fadeOut("slow");
